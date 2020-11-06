43de5a765cb932b3b7cc783eab3c425aeaeed08d
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-11-06 20:05:12 +0000

    Added crispy forms

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +79,11 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+         'builtins' : [
+             'crispy_forms.templatetags.crispy_forms_tags',
+             'crispy_forms.templatetags.crispy_forms_field',            
+         ]
+
         },
     },
 ]
