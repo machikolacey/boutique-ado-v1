083ab6190d0245981b905fd1fcc3107b239f8c7a
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-11-08 14:02:40 +0000

    Django countries

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
